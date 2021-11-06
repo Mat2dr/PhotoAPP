1d945ce25ef6e6b76223e86811c29f30fd239f3a
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-11-06 23:19:13 +0100

    Refactoring

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ function updateInput(e) {
     searchValue = e.target.value;
 }
 
-async function curatedPhotos() {
-    const dataFetch = await fetch('https://api.pexels.com/v1/curated/?page=1&per_page=16', 
+async function fetchApi(url){
+    const dataFetch = await fetch(url, 
     {
         method: 'GET',
         headers: {
@@ -27,6 +27,10 @@ async function curatedPhotos() {
         }
     });
     const data = await dataFetch.json();
+    return data;
+}
+
+function generatePictures(data){
     data.photos.forEach(photo => {
         const galleryImg = document.createElement('div');
         galleryImg.classList.add('gallery-img');
@@ -37,24 +41,16 @@ async function curatedPhotos() {
     });
 }
 
+async function curatedPhotos() {
+    const data = await fetchApi("https://api.pexels.com/v1/curated/?page=1&per_page=16");
+
+    generatePictures(data);
+}
+
 async function searchPhotos(query) {
-    const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=1`, 
-    {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            Authorization: auth
-        }
-    });
-    const data = await dataFetch.json();
-    data.photos.forEach(photo => {
-        const galleryImg = document.createElement('div');
-        galleryImg.classList.add('gallery-img');
-        galleryImg.innerHTML = `<img src=${photo.src.large}></img> 
-        <p>${photo.photographer}</p>
-        `;
-        gallery.appendChild(galleryImg);
-    });
+    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}&per_page=1`);
+    
+    generatePictures(data);
 }
 
 curatedPhotos();
